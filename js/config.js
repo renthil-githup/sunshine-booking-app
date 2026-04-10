95e54dd43abe79a72277543f73f122844a58a00d
renthil-githup <renthil@gmail.com>
2026-04-10 19:18:32 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const Config = {
     // URL for the backend server
     // Change this to your deployed cloud URL once deployed
-    API_BASE_URL: 'http://localhost:3000'
+    API_BASE_URL: 'https://sunshine-backend-w14m.onrender.com'
 };
 
 window.Config = Config;
